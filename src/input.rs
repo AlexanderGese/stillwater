f3f8b26be892e33140d95847a314452ccda24d60
Alexander Gese <alexander.gese07@gmail.com>
2026-08-15 21:59:24 +0200

    tests for input mapping

--- src/input.rs
+++ src/input.rs
@@ -19,3 +19,24 @@ pub fn key_to_action(code: KeyCode) -> Option<Action> {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::game::Action;
+    use crate::geom::Dir;
+    use crossterm::event::KeyCode;
+    #[test]
+    fn maps_keys() {
+        assert_eq!(key_to_action(KeyCode::Char('w')), Some(Action::Move(Dir::North)));
+        assert_eq!(key_to_action(KeyCode::Up), Some(Action::Move(Dir::North)));
+        assert_eq!(key_to_action(KeyCode::Char('d')), Some(Action::Move(Dir::East)));
+        assert_eq!(key_to_action(KeyCode::Char('e')), Some(Action::Interact));
+        assert_eq!(key_to_action(KeyCode::Char('j')), Some(Action::OpenJournal));
+        assert_eq!(key_to_action(KeyCode::Char('1')), Some(Action::Buy(1)));
+        assert_eq!(key_to_action(KeyCode::Char('6')), Some(Action::Buy(6)));
+        assert_eq!(key_to_action(KeyCode::Char('z')), Some(Action::Wait));
+        assert_eq!(key_to_action(KeyCode::Esc), Some(Action::Back));
+        assert_eq!(key_to_action(KeyCode::Char('q')), Some(Action::Quit));
+        assert_eq!(key_to_action(KeyCode::Char('x')), None);
+    }
+}
